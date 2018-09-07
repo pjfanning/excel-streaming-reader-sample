39c6eb8bc60bb34fa0c405fe77ff735bbd753c27
PJ Fanning <fanningpj@yahoo.com>
2018-09-07 20:03:23 +0100

    sample

---
+++ src/main/java/com/github/pjfanning/poi/sample/StreamingRead.java
@@ -0,0 +1,49 @@
+package com.github.pjfanning.poi.sample;
+
+import com.github.pjfanning.xlsx.StreamingReader;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.io.File;
+
+public class StreamingRead {
+
+    public static void main(String[] args) throws Exception {
+        String filename = null;
+        if (args.length < 1) {
+            System.out.println("To read your own file, use:");
+            System.out.println("  StreamingRead <xlsx file>");
+            System.out.println();
+            filename = "Sample-Sales-Data.xlsx";
+            System.out.println("Using " + filename + " as default input");
+        } else {
+            filename = args[0];
+        }
+
+        File xlsxFile = new File(filename);
+        if (!xlsxFile.exists()) {
+            System.err.println("Not found or not a file: " + xlsxFile.getPath());
+            return;
+        }
+
+        try (Workbook workbook = StreamingReader.builder()
+                .rowCacheSize(100)
+                .bufferSize(4096)
+                .sstCacheSize(1000)
+                .open(xlsxFile)) {
+            for (Sheet sheet : workbook){
+                System.out.println("Sheet: " + sheet.getSheetName());
+                for (Row r : sheet) {
+                    for (Cell c : r) {
+                        System.out.print('"');
+                        System.out.print(c.getStringCellValue());
+                        System.out.print("\",");
+                    }
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
